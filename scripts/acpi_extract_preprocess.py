0629283bb2f72b406797085ed0e17e7b7fa79ce7
Bruce Rogers <brogers@suse.com>
2019-06-27 10:15:24 -0600

    seabios: switch to python3 as needed
    Switch to python3 the places where "python2" is explicitly referenced.
    (Ignore the uses of #!/usr/bin/env python, since that usage does the
    right thing in our build environment).
    Include changes proposed by the python3 2to3 tool.
    
    Signed-off-by: Bruce Rogers <brogers@suse.com>

--- scripts/acpi_extract_preprocess.py
+++ scripts/acpi_extract_preprocess.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
 # Copyright (C) 2011 Red Hat, Inc., Michael S. Tsirkin <mst@redhat.com>
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
